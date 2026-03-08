a3d84017051d3f693231db90c57d78af405af704
Outman <38985402+kermit-o@users.noreply.github.com>
2026-03-08 01:50:12 +0000

    fix: block JitPack for Stripe deps using exclusiveContent filter

--- solva/plugins/withNoJitpack.js
+++ solva/plugins/withNoJitpack.js
@@ -1,12 +1,20 @@
-const { withSettingsGradle, withAppBuildGradle } = require('@expo/config-plugins');
+const { withAppBuildGradle, withSettingsGradle } = require('@expo/config-plugins');
 
-// Remove JitPack and force Stripe from Maven Central
 module.exports = function withNoJitpack(config) {
-  return withAppBuildGradle(config, (config) => {
+  // Step 1: Remove JitPack from settings.gradle
+  config = withSettingsGradle(config, (config) => {
+    config.modResults.contents = config.modResults.contents.replace(
+      /maven\s*\{\s*url\s*['"]https:\/\/jitpack\.io['"]\s*\}/g,
+      '// jitpack removed'
+    );
+    return config;
+  });
+
+  // Step 2: In app/build.gradle, force stripe versions AND exclude JitPack
+  config = withAppBuildGradle(config, (config) => {
     let contents = config.modResults.contents;
-    
-    // Pin all stripe deps and exclude jitpack lookups
-    const forceBlock = `
+
+    const block = `
 configurations.all {
     resolutionStrategy {
         force 'com.stripe:stripe-android:22.7.4'
@@ -15,12 +23,22 @@ configurations.all {
         force 'com.stripe:payments-ui-core:22.7.4'
         force 'com.stripe:stripe-ui-core:22.7.4'
     }
+    exclude group: 'com.jitpack'
+}
+
+repositories {
+    exclusiveContent {
+        forRepository { maven { url 'https://jitpack.io' } }
+        filter { excludeGroupByRegex 'com\\.stripe.*' }
+    }
 }
 `;
     if (!contents.includes('resolutionStrategy')) {
-      contents = contents.replace('dependencies {', forceBlock + '\ndependencies {');
+      contents = contents.replace('dependencies {', block + '\ndependencies {');
     }
     config.modResults.contents = contents;
     return config;
   });
+
+  return config;
 };
