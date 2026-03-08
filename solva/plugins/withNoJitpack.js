f082b65f44d307af09be89f0d3e52e02c07508b3
Outman <38985402+kermit-o@users.noreply.github.com>
2026-03-08 01:57:07 +0000

    fix: use double quotes in Gradle 9 resolutionStrategy

--- solva/plugins/withNoJitpack.js
+++ solva/plugins/withNoJitpack.js
@@ -1,7 +1,6 @@
 const { withAppBuildGradle, withSettingsGradle } = require('@expo/config-plugins');
 
 module.exports = function withNoJitpack(config) {
-  // Step 1: Remove JitPack from settings.gradle
   config = withSettingsGradle(config, (config) => {
     config.modResults.contents = config.modResults.contents.replace(
       /maven\s*\{\s*url\s*['"]https:\/\/jitpack\.io['"]\s*\}/g,
@@ -10,26 +9,17 @@ module.exports = function withNoJitpack(config) {
     return config;
   });
 
-  // Step 2: In app/build.gradle, force stripe versions AND exclude JitPack
   config = withAppBuildGradle(config, (config) => {
     let contents = config.modResults.contents;
 
     const block = `
 configurations.all {
     resolutionStrategy {
-        force 'com.stripe:stripe-android:22.7.4'
-        force 'com.stripe:financial-connections:22.7.4'
-        force 'com.stripe:payments-core:22.7.4'
-        force 'com.stripe:payments-ui-core:22.7.4'
-        force 'com.stripe:stripe-ui-core:22.7.4'
-    }
-    exclude group: 'com.jitpack'
-}
-
-repositories {
-    exclusiveContent {
-        forRepository { maven { url 'https://jitpack.io' } }
-        filter { excludeGroupByRegex 'com\\.stripe.*' }
+        force "com.stripe:stripe-android:22.7.4"
+        force "com.stripe:financial-connections:22.7.4"
+        force "com.stripe:payments-core:22.7.4"
+        force "com.stripe:payments-ui-core:22.7.4"
+        force "com.stripe:stripe-ui-core:22.7.4"
     }
 }
 `;
