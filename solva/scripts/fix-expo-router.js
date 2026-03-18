5fadadf5925ce1eda1faf2e569e53b081dca91e9
Outman <38985402+kermit-o@users.noreply.github.com>
2026-03-18 21:15:37 +0000

    revert: back to working state before NativeWind attempt

--- solva/scripts/fix-expo-router.js
+++
@@ -1,6 +0,0 @@
-
-const fs = require('fs');
-const original = fs.readFileSync(require('path').join(__dirname, '../node_modules/expo-router/_ctx.web.js'), 'utf8');
-const fixed = original.replace('process.env.EXPO_ROUTER_APP_ROOT', "'../app'").replace('process.env.EXPO_ROUTER_IMPORT_MODE', "'lazy'");
-fs.writeFileSync(require('path').join(__dirname, '../node_modules/expo-router/_ctx.web.js'), fixed);
-console.log('patched');
