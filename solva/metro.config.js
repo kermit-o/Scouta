67d43277e617471001f83d798b5cff83b2b814f2
Outman <38985402+kermit-o@users.noreply.github.com>
2026-03-19 21:43:06 +0000

    fix: remove custom resolverMainFields from metro config - was prioritizing react-native over browser modules

--- solva/metro.config.js
+++ solva/metro.config.js
@@ -1,4 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config')
 const config = getDefaultConfig(__dirname)
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main']
 module.exports = config
